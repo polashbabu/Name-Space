32a7f85a4c6ae5114db03fba490ac1ed655550e4
MD.Jahid Hassan <32840051+polashbabu@users.noreply.github.com>
2019-03-11 10:10:06 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using first;
+using second;
+
+
+namespace first
+{
+    class abc
+    {
+        public void function()
+        {
+            Console.WriteLine("first name space ");
+        }
+    }
+}
+namespace second
+{
+    class def
+    {
+        public void function()
+        {
+            Console.WriteLine("second name space ");
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        abc ob = new abc();
+        def obj = new def();
+        obj.function();
+        ob.function();
+        Console.ReadKey();
+
+
+
+    }
+}
